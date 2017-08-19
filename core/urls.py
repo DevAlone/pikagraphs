f74e9164c0f1d34103326c1a21d9c6523adc3a1f
DevAlone <dev@d3d.info>
2017-08-19 22:41:04 +0300

    fix

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ app_name = 'core'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^user/(?P<username>[a-z0-9]{1,20})/$', views.user, name='user'),
+    url(r'^user/(?P<username>[a-z0-9\._]{1,20})/$', views.user, name='user'),
 ]
