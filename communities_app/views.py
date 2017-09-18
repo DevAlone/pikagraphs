b233f976ab19cce30830d7c58c50a02442a7735e
DevAlone <admin@d3d.info>
2017-09-18 16:18:50 +0300

    fix

--- communities_app/views.py
+++ communities_app/views.py
@@ -48,8 +48,8 @@ def secret_page_for_lactarius(request):
     lastDay = 0
     for entry in countersEntries:
         daysSinceEpoch = (datetime.datetime.utcfromtimestamp(
-            entry.timestamp - 3600 * 3) -
-            datetime.datetime.utcfromtimestamp(-3600 * 3)
+            entry.timestamp + 3600 * 3) -
+            datetime.datetime.utcfromtimestamp(0)
         ).days
 
         if daysSinceEpoch > lastDay:
