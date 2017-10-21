649a1171b4b46b75aed586dac815164941dce40b
DevAlone <admin@d3d.info>
2017-10-22 02:52:54 +0300

    add loading animation

--- static/core/js/main.js
+++ static/core/js/main.js
@@ -9,3 +9,23 @@ function hidePopup()
     popup.setAttribute('style','display: none');
     document.body.style.overflow = "auto"
 }
+
+function _showLoadingAnimation(elem)
+{
+    if (typeof loading_element !== 'undefined') {
+        loading_element.textContent += '.';
+        setTimeout(_showLoadingAnimation, 1000);
+    }
+}
+
+function showLoadingAnimation(elem)
+{
+    elem.innerHTML = '<h1 id="loading_element">Загрузка</div>';
+
+    _showLoadingAnimation(elem);
+}
+
+function showError(elem, text)
+{
+    elem.innerHTML = '<h1 id="error_element">' + text + '</div>';
+}
