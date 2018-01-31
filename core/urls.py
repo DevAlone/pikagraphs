29e55d4f719a913c73473c63115bb5671df71a9d
DevAlone <admin@d3d.info>
2018-01-31 21:41:03 +0300

    fix urls

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,8 @@ urlpatterns = [
     # url(r'^$', views.users, name='index'),
     # url(r'^users/$', views.users, name='users'),
     # url(r'^user/(?P<username>[a-zA-Z0-9\._]{1,20})/$', views.user, name='user'),
-    # url(r'^secret_page_for_l4rever/$', views.secret_page_for_l4rever,
-    #     name='secret_page_for_l4rever'),
+    url(r'^secret_page_for_l4rever/$', views.secret_page_for_l4rever,
+        name='secret_page_for_l4rever'),
 
     # url(r'^rest/', include(router.urls)),
 ]
--- pikabot_graphs/urls.py
+++ pikabot_graphs/urls.py
@@ -5,16 +5,18 @@ from django.conf.urls.static import static
 from django.contrib import admin
 import django
 import core.views
+import communities_app.views
 import api.views
 
 
 urlpatterns = [
-    url(r'^', include('core.urls')),
-    url(r'^api/', include('api.urls')),
-    # url(r'^very_secret_admin_page_nobody_knows_this_address/', admin.site.urls),
-    # url(r'^communities/', include('communities_app.urls')),
-    # url(r'^pikabu_new_year_18_game_app/', include('pikabu_new_year_18_game_app.urls')),
-    # url(r'^', include('fakeadmin.urls')),
+    url(r'^', include('api.urls')),
+    url(r'^secret_page_for_lactarius/$', communities_app.views.secret_page_for_lactarius,
+        name='secret_page_for_lactarius'),
+    url(r'^secret_page_for_l4rever/$', core.views.secret_page_for_l4rever,
+        name='secret_page_for_l4rever'),
+
+    url(r'^', include('fakeadmin.urls')),
 ]
 
 
