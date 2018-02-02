9d969e75a78cf072036b658878ac4ae97ef6e218
DevAlone <admin@d3d.info>
2018-02-02 18:19:16 +0300

    change page size

--- pikabot_graphs/_settings.py
+++ pikabot_graphs/_settings.py
@@ -168,5 +168,5 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 50,
 }
