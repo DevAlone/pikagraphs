f62f08105af6707ffc7029c86b710f7ec313a585
DevAlone <admin@d3d.info>
2018-02-06 11:45:54 +0300

    fix

--- core/models.py
+++ core/models.py
@@ -16,15 +16,15 @@ class User(models.Model):
     pluses_count = models.IntegerField(default=0, db_index=True)
     minuses_count = models.IntegerField(default=0, db_index=True)
     subscribers_count = models.IntegerField(default=0, db_index=True)
-    is_rating_ban = models.BooleanField(default=False, db_index=True)
-    gender = models.CharField(max_length=1, default='-', db_index=True)
+    is_rating_ban = models.BooleanField(default=False)
+    gender = models.CharField(max_length=1, default='-')
     approved = models.TextField(default="")
     awards = models.TextField(default="")
     signup_timestamp = models.IntegerField(default=0, db_index=True)
 
     info = models.TextField(blank=True, null=True)
 
-    updating_period = models.IntegerField(default=60, db_index=True)
+    updating_period = models.IntegerField(default=60)
     is_updated = models.BooleanField(default=False, db_index=True)
 
     last_update_timestamp = models.IntegerField(default=0, db_index=True)
